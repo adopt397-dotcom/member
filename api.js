e2e4a8b881db32d7dce8a6795298a038aeaf490e
adopt397-dotcom <adopt397@gmail.com>
2026-07-07 18:13:39 -0700

    Update api.js

--- api.js
+++ api.js
@@ -1,8 +1,28 @@
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxmqRaTC5SlbstqyWcshgfEt99HyomTMq0BAZr5aXFp9EDJNGRIy1pgRZJA3-xNrm29/exec';
+
 window.login = async function(email, pin) {
-  const response = await fetch('https://script.google.com/macros/s/AKfycbxmqRaTC5SlbstqyWcshgfEt99HyomTMq0BAZr5aXFp9EDJNGRIy1pgRZJA3-xNrm29/exec', {
+  const response = await fetch(GAS_URL, {
     method: 'POST',
     headers: { 'Content-Type': 'text/plain' },
-    body: JSON.stringify({ email, pin })
+    body: JSON.stringify({ action: 'login', email, pin })
+  });
+  return response.json();
+};
+
+window.signup = async function(email, pin, name) {
+  const response = await fetch(GAS_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'text/plain' },
+    body: JSON.stringify({ action: 'signup', email, pin, name })
+  });
+  return response.json();
+};
+
+window.changePin = async function(email, oldPin, newPin) {
+  const response = await fetch(GAS_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'text/plain' },
+    body: JSON.stringify({ action: 'changePin', email, oldPin, newPin })
   });
   return response.json();
 };
